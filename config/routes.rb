56ca789352d63dd3d7b2fdc55aa376e38e07bfc9
Cronon <cronon12@gmail.com>
2013-08-27 02:45:42 +0300

    add beautiful view

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Fanfiction::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'books#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
