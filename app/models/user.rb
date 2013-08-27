fd555b04955115c557bd1b5cb4c372f33c5e664a
Cronon <cronon12@gmail.com>
2013-08-27 04:02:14 +0300

    connect users and books

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+  has_many :books, dependent: :destroy       
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130826224210) do
+ActiveRecord::Schema.define(version: 20130827004723) do
 
   create_table "books", force: true do |t|
     t.string   "title"
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                            default: "",     null: false
-    t.string   "encrypted_password",               default: "",     null: false
+    t.string   "email",                  default: "",     null: false
+    t.string   "encrypted_password",     default: "",     null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                    default: 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -34,8 +35,7 @@ ActiveRecord::Schema.define(version: 20130826224210) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
-    t.binary   "admin",                  limit: 1, default: "b'0'"
-    t.string   "role",                             default: "user"
+    t.string   "role",                   default: "user"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
