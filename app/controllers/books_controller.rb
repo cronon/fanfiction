e26e9eee73f0a9a4fe498c1c264a6595ac297866
Cronon <cronon12@gmail.com>
2013-08-31 09:28:53 +0300

    categories selector

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -99,6 +99,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:title, :description, :tag_list)
+      params.require(:book).permit(:title, :description, :tag_list, :category)
     end
 end
