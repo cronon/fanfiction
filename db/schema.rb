308a027ea6b7794d8c5c15878f2d921671385159
Cronon <cronon12@gmail.com>
2013-08-31 14:02:56 +0300

    light theme

--- db/schema.rb
+++ db/schema.rb
@@ -128,8 +128,8 @@ ActiveRecord::Schema.define(version: 20130829124301861) do
   end
 
   create_table "users", force: true do |t|
-    t.string   "email",                  default: "",     null: false
-    t.string   "encrypted_password",     default: "",     null: false
+    t.string   "email",                  default: "",          null: false
+    t.string   "encrypted_password",     default: "",          null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -146,6 +146,7 @@ ActiveRecord::Schema.define(version: 20130829124301861) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.string   "role",                   default: "user"
+    t.string   "theme",                  default: "light.css"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
